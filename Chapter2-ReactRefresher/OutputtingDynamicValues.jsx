bae9c99d21896d9ceecf760ca968589bdd4ca8e7
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-09-19 11:55:27 -0500

    Update OutputtingDynamicValues.jsx

--- Chapter2-ReactRefresher/OutputtingDynamicValues.jsx
+++ Chapter2-ReactRefresher/OutputtingDynamicValues.jsx
@@ -3,12 +3,9 @@ const names = ['Maximilian','Manuel'];
 function Post() 
 {
     const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-    return
-    (
-      <div>
-        <p>VIBHINAV</p>
-        <p>React is Boring!</p>
-      </div>
+    return(
+        <p>{chosenName}</p>
     );
+
 }
   export default Post;
