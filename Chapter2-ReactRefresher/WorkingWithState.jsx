39210a5872c9e91c4cf8e696b8dd960a9677c8dc
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-09-26 11:06:10 -0500

    Update WorkingWithState.jsx

--- Chapter2-ReactRefresher/WorkingWithState.jsx
+++ Chapter2-ReactRefresher/WorkingWithState.jsx
@@ -9,9 +9,17 @@ function WorkingWithState() {
   }
 
   return(
-    
+        <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+
   )
-
-
-
 }
