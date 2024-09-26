408797e8ef01b740cd70512bc646ac998cb2ea3d
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-09-26 09:09:03 -0500

    Update CSSstylesAndModules.jsx

--- Chapter2-ReactRefresher/CSSstylesAndModules.jsx
+++ Chapter2-ReactRefresher/CSSstylesAndModules.jsx
@@ -1,4 +1,15 @@
 //create file called post.module.css
 
 //post.jsx
+import classes from './Post.module.css'
+function Post(props) 
+{
+    return(
+      <div classname = {classes.post}>
+        <p>{props.author}</p>
+        <p>{props.body}</p>
+      </div>  
+    );
 
+}
+  export default Post;
