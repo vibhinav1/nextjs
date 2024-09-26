1ceeb822c1c807dbf47d967ae433c4189bafc8ae
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-09-26 11:38:21 -0500

    Create LiftingStateUp.jsx

---
+++ Chapter2-ReactRefresher/LiftingStateUp.jsx
@@ -0,0 +1,68 @@
+//newpost.jsx
+import classes from './NewPost.module.css';
+
+function NewPost(props) {
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
+
+//post.jsx
+
+import classes from './Post.module.css';
+
+function Post(props) {
+  return (
+    <li className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </li>
+  );
+}
+
+export default Post;
+
+//postlist.jsx
+
+
+import { useState } from 'react';
+
+import Post from './Post';
+import NewPost from './NewPost';
+import classes from './PostsList.module.css';
+
+function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
