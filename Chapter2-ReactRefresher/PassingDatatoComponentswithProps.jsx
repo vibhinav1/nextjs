dff5cd4ee706cd6330b1edce1b054e895ccff729
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-09-26 08:30:50 -0500

    Update PassingDatatoComponentswithProps.jsx

--- Chapter2-ReactRefresher/PassingDatatoComponentswithProps.jsx
+++ Chapter2-ReactRefresher/PassingDatatoComponentswithProps.jsx
@@ -1,3 +1,18 @@
 //app.jsx
 
+import Post from './components/Post';
+
+function App() {
+  return (
+  <main>
+    <Post author ="maximilian" body = "react is awesome" />
+    <Post />
+    <Post />
+  </main>
+  );
+}
+  export default App;
+
+//post.jsx
+
 
